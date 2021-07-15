030435725609c919d4887ed2db324e6ed1b53c1c
hassan - mac <hne1991@gmail.com>
2021-07-15 15:00:10 +0430

    init commit

---
+++ precache-manifest.31981db8e6814f253ef8143a67d153d9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "695e661b87cf38fee8b0bc4b43fee7a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72cd828a1b213571d9b9",
+    "url": "/static/css/main.2aa6ba03.chunk.css"
+  },
+  {
+    "revision": "4addd047412f14519837",
+    "url": "/static/js/2.3e5153a7.chunk.js"
+  },
+  {
+    "revision": "72cd828a1b213571d9b9",
+    "url": "/static/js/main.90558c65.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "31ad3099071d0981f5bfcc2d480ee731",
+    "url": "/static/media/Shabnam.31ad3099.ttf"
+  },
+  {
+    "revision": "46e7d4169d218ddfaa17c5e49805e9a2",
+    "url": "/static/media/Shabnam.46e7d416.woff"
+  },
+  {
+    "revision": "7b23666bae662ec6d8d7c66c4e2b2964",
+    "url": "/static/media/Shabnam.7b23666b.eot"
+  },
+  {
+    "revision": "dc59fa37f0317ef9ad0019d018a74483",
+    "url": "/static/media/Shabnam.dc59fa37.woff2"
+  },
+  {
+    "revision": "e381a31537bae74fc447ea571937e897",
+    "url": "/static/media/iseeco.e381a315.png"
+  },
+  {
+    "revision": "12e7c962faf7052abf9ac38adcb76dfa",
+    "url": "/static/media/window-active.12e7c962.png"
+  },
+  {
+    "revision": "01c9790dbda4d76e8cf518c164bc39f9",
+    "url": "/static/media/window-dark.01c9790d.png"
+  }
+]);
